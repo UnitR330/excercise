fe8929c406d0895a30425875b16c19ee519bd139
UnitR330 <145355731+UnitR330@users.noreply.github.com>
2023-10-11 13:21:22 +0300

    Update

--- fridaychallenge/fr2.js
+++ fridaychallenge/fr2.js
@@ -1,27 +1,27 @@
 // 1 First
-const account1 = 275;
-const accountValue1 = account1 >= 50 && account1 <= 300 ? 0.15 : 0.2;
-const tips1 = account1 * accountValue1;
-const totalValue1 = account1 + tips1;
+const invoice1 = 275;
+const invoiceValue1 = invoice1 >= 50 && invoice1 <= 300 ? 0.15 : 0.2;
+const tips1 = invoice1 * invoiceValue1;
+const totalValue1 = invoice1 + tips1;
 
 // Display
-const row1 = `Account has ${account1}, tips ${tips1}, total sum ${totalValue1}`;
+const row1 = `Invoice ${invoice1}, tips ${tips1}, Total sum ${totalValue1}`;
 console.log(row1);
 
 // 2 Second
-const account2 = 40;
-const accountValue2 = account2 >= 50 && account2 <= 300 ? 0.15 : 0.2;
-const tips2 = account2 * accountValue2;
-const totalValue2 = account2 + tips2;
+const invoice2 = 40;
+const invoiceValue2 = invoice2 >= 50 && invoice2 <= 300 ? 0.15 : 0.2;
+const tips2 = invoice2 * invoiceValue2;
+const totalValue2 = invoice2 + tips2;
 
-const row2 = `Account has ${account2}, tips ${tips2}, total sum ${totalValue2}`;
+const row2 = `Invoice ${invoice2}, tips ${tips2}, Total sum ${totalValue2}`;
 console.log(row2);
 
 // 3 Third
-const account3 = 430;
-const accountValue3 = account3 >= 50 && account3 <= 300 ? 0.15 : 0.2;
-const tips3 = account3 * accountValue3;
-const totalValue3 = account3 + tips3;
+const invoice3 = 430;
+const invoiceValue3 = invoice3 >= 50 && invoice3 <= 300 ? 0.15 : 0.2;
+const tips3 = invoice3 * invoiceValue3;
+const totalValue3 = invoice3 + tips3;
 
-const row3 = `Account has ${account3}, tips ${tips3}, total sum ${totalValue3}`;
+const row3 = `Invoice ${invoice3}, tips ${tips3}, Total sum ${totalValue3}`;
 console.log(row3);
