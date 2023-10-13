679261d6e4230fafb097a8d7e4774a8b5d725c9f
UnitR330 <145355731+UnitR330@users.noreply.github.com>
2023-10-13 23:09:36 +0300

    Update

--- fridaychallenge/fr3.js
+++ fridaychallenge/fr3.js
@@ -68,8 +68,8 @@ function replaceLetters(string) {
   const modifiedString = replaceLetters(originalString);
   console.log(modifiedString);
 
-// 5
-
+// 5 
+/*
  function rand(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -90,6 +90,78 @@ console.log(`f 4: ${f4}`);
 
 // 5*
 
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const f1 = rand(0, 2);
+const f2 = rand(0, 2);
+const f3 = rand(0, 2);
+const f4 = rand(0, 2);
+
+// Count occurrences
+let count0 = 0;
+let count1 = 0;
+let count2 = 0;
+
+// Update counts based on values
+[count0, count1, count2] = [f1, f2, f3, f4].reduce((counts, value) => {
+    counts[value]++;
+    return counts;
+}, [count0, count1, count2]);
+
+console.log(`f 1: ${f1}`);
+console.log(`f 2: ${f2}`);
+console.log(`f 3: ${f3}`);
+console.log(`f 4: ${f4}`);
+console.log(`Count of 0: ${count0}`);
+console.log(`Count of 1: ${count1}`);
+console.log(`Count of 2: ${count2}`);
+*/
+// 5 **
+
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const f1 = rand(0, 2);
+const f2 = rand(0, 2);
+const f3 = rand(0, 2);
+const f4 = rand(0, 2);
+
+// Count the occurrences of each value
+let countZero = 0;
+let countOne = 0;
+let countTwo = 0;
+
+if (f1 === 0) countZero++;
+else if (f1 === 1) countOne++;
+else if (f1 === 2) countTwo++;
+
+if (f2 === 0) countZero++;
+else if (f2 === 1) countOne++;
+else if (f2 === 2) countTwo++;
+
+if (f3 === 0) countZero++;
+else if (f3 === 1) countOne++;
+else if (f3 === 2) countTwo++;
+
+if (f4 === 0) countZero++;
+else if (f4 === 1) countOne++;
+else if (f4 === 2) countTwo++;
+
+// Print the results
+console.log(`f 1: ${f1}`);
+console.log(`f 2: ${f2}`);
+console.log(`f 3: ${f3}`);
+console.log(`f 4: ${f4}`);
+console.log(`Count of Zeros: ${countZero}`);
+console.log(`Count of Ones: ${countOne}`);
+console.log(`Count of Twos: ${countTwo}`);
 
 
 // 6
